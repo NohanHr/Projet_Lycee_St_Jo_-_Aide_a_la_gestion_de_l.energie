9b8c6985ff5b08e880745652e214939e6513848f
elouan23 <legall.e1@stjolorient.fr>
2025-05-20 09:15:17 +0200

    Update auth.php

--- auth.php
+++ auth.php
@@ -1,9 +1,19 @@
 <?php
 session_start();
+$pdo = new PDO('mysql:host=localhost;dbname=energie;charset=utf8', 'admin', 'ciel2');
 
-// Redirige vers login.php si non connecté
-if (!isset($_SESSION["user"])) {
-    header("Location: login.php");
-    exit();
+// Vérifie si l'utilisateur est connecté
+function estConnecte() {
+    return isset($_SESSION['user']);
+}
+
+// Vérifie si l'utilisateur est admin
+function estAdmin() {
+    return estConnecte() && $_SESSION['user']['role'] === 'admin';
+}
+
+// Vérifie si l'utilisateur est visiteur
+function estVisiteur() {
+    return estConnecte() && $_SESSION['user']['role'] === 'user';
 }
 ?>
